c67c6d3532723a8a6e4b45b8156092094f382555
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2024-02-29 00:11:37 +0100

    Working.

--- start.py
+++ start.py
@@ -2,9 +2,12 @@ from claid import CLAID
 
 from claid.module.module_factory import ModuleFactory
 
-from HARModule import HARModule
 
 claid = CLAID()
+# Do this before importing tensorflow!!
+claid.hello_world()
+
+from HARModule import HARModule
 
 module_factory = ModuleFactory()
 module_factory.register_module(HARModule)
