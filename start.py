061e7360c7d931a58aa0cb0225c0f5db0033eec1
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2024-03-01 17:42:56 +0100

    Working.

--- start.py
+++ start.py
@@ -1,14 +1,16 @@
 from claid import CLAID
-
+from claid_designer import CLAIDDesigner
 from claid.module.module_factory import ModuleFactory
 
 
 claid = CLAID()
+designer = CLAIDDesigner()
+designer.attach(claid)
 # Do this before importing tensorflow!!
 claid.hello_world()
 
-from HARModule import HARModule
 
 module_factory = ModuleFactory()
-module_factory.register_module(HARModule)
-claid.start("claid_config.json", "workshop_laptop", "user", "device", module_factory)
+module_factory.register_all_modules_found_in_path("my_modules")
+module_factory.print_registered_modules()
+claid.start("empty_workshop_config.json", "Laptop", "user", "device", module_factory)
