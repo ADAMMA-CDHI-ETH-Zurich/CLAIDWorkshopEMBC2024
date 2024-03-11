bc8e99435a3c84fa710a20b3f489428a38163052
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2024-03-11 14:19:30 +0100

    Added default modules.

--- start.py
+++ start.py
@@ -9,5 +9,5 @@ claid.hello_world()
 
 module_factory = ModuleFactory()
 module_factory.register_all_modules_found_in_path("my_modules")
-
+module_factory.register_default_modules()
 designer.start("Laptop", "laptop_user", "device", module_factory)
