6682671f1027061c992bb2540876e0a5a6d55af1
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2024-03-13 13:32:22 +0100

    Added.

---
+++ launch_claid_and_designer.py
@@ -0,0 +1,14 @@
+from claid import CLAID
+from claid_designer import CLAIDDesigner
+from claid.module.module_factory import ModuleFactory
+
+claid = CLAID()
+designer = CLAIDDesigner()
+designer.attach(claid)
+claid.hello_world()
+
+module_factory = ModuleFactory()
+module_factory.register_default_modules()
+module_factory.register_all_modules_found_in_path("my_modules")
+
+designer.start("Laptop", "laptop_user", "device", module_factory)
